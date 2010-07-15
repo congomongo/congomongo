489d41ea9c4a7e0b34fed3393ae482afeac6d454
Steve Purcell <steve@sanityinc.com>
2010-07-15 14:00:36 +0100

    Update mongo java driver to 2.0, and make corresponding fixes.
    Note that metadata on gridfs files is now stored directly on the file object,
    not inside a :metadata sub-map.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "clojure-friendly api for MongoDB"
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                 [org.clojars.somnium/mongo-java-driver "1.1.0-SNAPSHOT"]
+                 [org.mongodb/mongo-java-driver "2.0"]
                  [org.clojars.somnium/clojure-db-object "0.1.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]
                      [org.clojars.somnium/user "0.1.0-SNAPSHOT"]])
