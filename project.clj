2e60ba02ea20be48e82de6d1811bc8f02082cf14
Andrew Boekhoff <you@expample.com>
2010-07-22 09:39:28 -0400

    remove user.clj from project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [org.mongodb/mongo-java-driver "2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
