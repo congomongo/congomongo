46310ef29414b8f6c913bfc2347df2b2480a4bd0
Andrew Boekhoff <you@expample.com>
2010-07-22 09:33:19 -0400

    remove user.clj from dev-dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [org.mongodb/mongo-java-driver "2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [org.clojars.somnium/user "0.1.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.2.1"])
