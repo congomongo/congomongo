2c9d5249872c106a9e8f09cd11a45dce9fc37697
Heinz N. Gies <heinz@licenser.net>
2011-01-24 17:05:33 +0800

    Clojure 1.2 is now stable.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject congomongo
   "0.1.3-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.mongodb/mongo-java-driver "2.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
