89b32b6384b5a9cec335f5af55c06e7f7832840d
Steve Purcell <steve@sanityinc.com>
2011-03-30 14:12:51 +0100

    Update dev-dependencies to swank-clojure 1.3.0

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.mongodb/mongo-java-driver "2.4"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.3.0"]])
