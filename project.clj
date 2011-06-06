e404f26470b64da7e35f59f5d3bcbce47872fb0b
Steve Purcell <steve@sanityinc.com>
2011-06-06 10:41:08 +0100

    Bump clojure and swank-clojure jar versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject congomongo
   "0.1.4-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.mongodb/mongo-java-driver "2.5.3"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]])
+  :dev-dependencies [[swank-clojure "1.3.1"]])
