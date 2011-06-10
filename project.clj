c1d143d7be3c2436017948efad2e668bd80ea191
Sean Corfield <sean@corfield.org>
2011-06-10 16:38:36 -0700

    Switched dependencies from old contrib to new contrib.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject congomongo
   "0.1.4-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [;; Also tested with: [org.clojure/clojure "1.3.0-alpha8"]
+                 [org.clojure/clojure "1.2.1"]
+                 [org.clojure/core.incubator "0.1.0"]
+                 [org.clojure/data.json "0.1.0"]
                  [org.mongodb/mongo-java-driver "2.6.2"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
