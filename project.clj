b23f5941a139a6f91ca22afcaf79798a4d51ed47
Sean Corfield <sean@corfield.org>
2011-08-04 16:17:07 -0700

    Bump to 0.1.7-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject congomongo
-  "0.1.6"
+  "0.1.7-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
   :dependencies [;; Also tested with: [org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/clojure "1.2.1"]
