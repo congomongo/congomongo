50a34b550e373b28ff0e9df8e7d8ca63b010ab1c
Sean Corfield <sean@corfield.org>
2011-08-11 10:49:53 -0700

    Update Java driver to 2.6.5

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.0"]
-                 [org.mongodb/mongo-java-driver "2.6.3"]]
+                 [org.mongodb/mongo-java-driver "2.6.5"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
