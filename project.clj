f036937d6375d8b4c117fbebe04b5678cf111d3a
Sean Corfield <sean@worldsingles.com>
2011-09-07 22:34:46 -0700

    Update data.json version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :dependencies [;; Also tested with: [org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [org.clojure/data.json "0.1.0"]
+                 [org.clojure/data.json "0.1.1"]
                  [org.mongodb/mongo-java-driver "2.6.5"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
