3574c93313980cd3045a61a6faca95a287175af8
Sean Corfield <sean@corfield.org>
2011-10-14 18:53:41 -0700

    Update to clojure.data.json 0.1.2 which includes a number of performance improvements

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
                  ;; and: [org.clojure/clojure "1.4.0-alpha1"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [org.clojure/data.json "0.1.1"]
+                 [org.clojure/data.json "0.1.2"]
                  [org.mongodb/mongo-java-driver "2.6.5"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
