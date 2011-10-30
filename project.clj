69fab75aa29c61c107ce9ea57f842c2d4230b1c5
Steve Purcell <steve@sanityinc.com>
2011-10-30 13:50:43 +0000

    Specify range of acceptable clojure versions (fixes #39)

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject congomongo
   "0.1.8-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
-  :dependencies [;; Also tested with: [org.clojure/clojure "1.3.0"]
-                 ;; and: [org.clojure/clojure "1.4.0-alpha1"]
-                 [org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "[1.2.1,1.4.0-alpha1]"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.mongodb/mongo-java-driver "2.6.5"]]
