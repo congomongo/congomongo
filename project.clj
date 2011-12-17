9bd4dfbe6c2c51ea2eb69f977eec7c116688f758
Sean Corfield <sean@corfield.org>
2011-12-17 15:43:05 -0800

    Extended Clojure dependency range up to 1.4.0 Alpha 3.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject congomongo
   "0.1.8-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
-  :dependencies [[org.clojure/clojure "[1.2.1,1.4.0-alpha1]"]
+  :dependencies [[org.clojure/clojure "[1.2.1,1.4.0-alpha3]"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.mongodb/mongo-java-driver "2.6.5"]]
