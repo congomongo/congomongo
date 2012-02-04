2e47f183a17eac37f8be564d6e5be97659b4f6c7
Max Penet <max.penet@gmail.com>
2012-02-05 00:13:23 +0100

    update mongo-java-driver to "2.7.3"

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :dependencies [[org.clojure/clojure "[1.2.1,1.4.0-alpha3]"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [org.mongodb/mongo-java-driver "2.6.5"]]
+                 [org.mongodb/mongo-java-driver "2.7.3"]]
   :dev-dependencies [[swank-clojure "1.3.1"]])
