1bc2da5388481ff5c71c7e49230e81915b9b038d
Sean Corfield <sean@corfield.org>
2012-03-10 23:04:23 -0800

    Update multi test to 1.4.0 beta 4.

--- project.clj
+++ project.clj
@@ -11,5 +11,5 @@
                      [swank-clojure "1.4.0"]]
   :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
   :multi-deps {"1.2" ~(conj deps '[org.clojure/clojure "1.2.1"])
-               "1.4B" ~(conj deps '[org.clojure/clojure "1.4.0-beta1"])}
+               "1.4B" ~(conj deps '[org.clojure/clojure "1.4.0-beta4"])}
 )
