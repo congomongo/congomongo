dc26d8680ac28bf5fc704f671ca6cf66e19e2db8
Sean Corfield <sean@corfield.org>
2012-04-15 16:11:43 -0700

    Add sonatype snapshots for Clojure 1.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
   :description "clojure-friendly api for MongoDB"
   :dev-dependencies [[lein-multi "1.1.0"]
                      [swank-clojure "1.4.2"]]
+  :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
   :multi-deps {"1.2"  ~(conj deps '[org.clojure/clojure "1.2.1"])
                "1.4"  ~(conj deps '[org.clojure/clojure "1.4.0"])
