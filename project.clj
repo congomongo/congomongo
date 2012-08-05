205d642eb696854ae4d9f6b46643ee5438221786
Sean Corfield <sean@corfield.org>
2012-08-05 11:59:37 -0700

    Add URL / license / mailing list info to project.clj for Clojars.

--- project.clj
+++ project.clj
@@ -7,6 +7,13 @@
 (defproject congomongo
   "0.1.11-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
+  :url "https://github.com/aboekhoff/congomongo"
+  :mailing-list {:name "congomongo mailing list"
+                 :archive "https://groups.google.com/forum/?fromgroups#!forum/congomongo-dev"
+                 :post "congomongo-dev@googlegroups.com"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"
+            :distribution :repo}
   :dev-dependencies [[lein-multi "1.1.0"]
                      [swank-clojure "1.4.2"]]
   :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
