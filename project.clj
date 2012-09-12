3df2c970294872db2f02882f41632c0fdedd62f5
Sean Corfield <sean@corfield.org>
2012-09-12 11:11:16 -0700

    Fix #1 by upgrading to Leiningen 2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,3 @@
-;; common dependencies:
-(def deps '[[org.clojure/core.incubator "0.1.0"]
-            [org.clojure/data.json "0.1.3"]
-            [org.mongodb/mongo-java-driver "2.8.0"]])
-
-;; project definition for multi-version testing:
 (defproject congomongo
   "0.1.11-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
@@ -14,10 +8,16 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :dev-dependencies [[lein-multi "1.1.0"]
-                     [swank-clojure "1.4.2"]]
-  :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-  :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
-  :multi-deps {"1.2"  ~(conj deps '[org.clojure/clojure "1.2.1"])
-               "1.4"  ~(conj deps '[org.clojure/clojure "1.4.0"])
-               "1.5S" ~(conj deps '[org.clojure/clojure "1.5.0-master-SNAPSHOT"])})
+  :min-lein-version "2.0.0"
+  :plugins [[lein-swank "1.4.4"]]
+  :dependencies [[org.clojure/core.incubator "0.1.0"]
+                 [org.clojure/data.json "0.1.3"]
+                 [org.mongodb/mongo-java-driver "2.8.0"]
+                 [org.clojure/clojure "1.3.0"]]
+  ;; if a :dev profile is added, remember to update :aliases below to
+  ;; use it in each with-profile group!
+  :profiles {:1.2  {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5S {:repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+                    :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "default:1.2,default:1.4,default:1.5S,default" "test"]})
