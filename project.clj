debd6919e18a8b612e8a004afe13cf7dd2fd0f59
Sean Corfield <sean@corfield.org>
2014-02-25 17:48:03 -0800

    Library updates and Clojure 1.6 testing.
    * core.incubator 0.1.2 -> 0.1.3
    * data.json 0.2.1 -> 0.2.4
    * mongo-java-driver 2.10.1 -> 2.11.4
    * clojure 1.4.0 -> 1.5.1 (default version)
    * tests against 1.3.0, 1.4.0, 1.5.1, 1.6.0-beta1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject congomongo
-  "0.4.2-SNAPSHOT"
+  "0.4.2"
   :description "Clojure-friendly API for MongoDB"
   :url "https://github.com/aboekhoff/congomongo"
   :mailing-list {:name "CongoMongo mailing list"
@@ -9,12 +9,13 @@
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/core.incubator "0.1.2"]
-                 [org.clojure/data.json "0.2.1"]
-                 [org.mongodb/mongo-java-driver "2.10.1"]
-                 [org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/core.incubator "0.1.3"]
+                 [org.clojure/data.json "0.2.4"]
+                 [org.mongodb/mongo-java-driver "2.11.4"]
+                 [org.clojure/clojure "1.5.1"]]
   ;; if a :dev profile is added, remember to update :aliases below to
   ;; use it in each with-profile group!
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :aliases {"test-all" ["with-profile" "default:1.3,default:1.5,default" "test"]})
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}}
+  :aliases {"test-all" ["with-profile" "default:1.3,default:1.4,default,default:1.6" "test"]})
