8489e092c6837a1681eb0f4b672895f7533293a5
Sean Corfield <sean@corfield.org>
2014-04-08 18:02:10 -0700

    Shift default version to Clojure 1.6.0; still testing against 1.3.0, 1.4.0, 1.5.1 as well; bump MongoDB driver to 2.12.0 release.

--- project.clj
+++ project.clj
@@ -11,11 +11,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.json "0.2.4"]
-                 [org.mongodb/mongo-java-driver "2.11.4"]
-                 [org.clojure/clojure "1.5.1"]]
+                 [org.mongodb/mongo-java-driver "2.12.0"]
+                 [org.clojure/clojure "1.6.0"]]
   ;; if a :dev profile is added, remember to update :aliases below to
   ;; use it in each with-profile group!
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}}
-  :aliases {"test-all" ["with-profile" "default:1.3,default:1.4,default,default:1.6" "test"]})
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :aliases {"test-all" ["with-profile" "default:1.3,default:1.4,default:1.5,default" "test"]})
