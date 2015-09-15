394741b17db0f0f0d3df0aaf4a0a17f3389184e0
Gordon Syme <gordonsyme@gmail.com>
2015-09-15 16:18:58 +0100

    ensureIndex is deprecated, replaced by createIndex

--- src/somnium/congomongo.clj
+++ src/somnium/congomongo.clj
@@ -600,7 +600,7 @@ You should use fetch with :limit 1 instead."))); one? and sort should NEVER be c
    [c f & {:keys [name unique sparse background partial-filter-expression]
            :or {name nil unique false sparse false background false}}]
    (-> (get-coll c)
-       (.ensureIndex (coerce-index-fields f) ^DBObject (coerce (merge {:unique unique :sparse sparse :background background}
+       (.createIndex (coerce-index-fields f) ^DBObject (coerce (merge {:unique unique :sparse sparse :background background}
                                                                        (if name {:name name})
                                                                        (if partial-filter-expression
                                                                          {:partialFilterExpression partial-filter-expression}))
