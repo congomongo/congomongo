6db0c57f6d23ce08433903c3c40105ce2ce8042a
Sean Corfield <sean@corfield.org>
2016-02-25 10:05:59 -0800

    Bump Clojure default to 1.8.0; add profile for 1.7.0

--- project.clj
+++ project.clj
@@ -12,12 +12,12 @@
   :dependencies [[org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.json "0.2.6"]
                  [org.mongodb/mongo-java-driver "2.14.0"]
-                 [org.clojure/clojure "1.7.0"]]
+                 [org.clojure/clojure "1.8.0"]]
   ;; if a :dev profile is added, remember to update :aliases below to
   ;; use it in each with-profile group!
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-alpha2"]]}}
-  :aliases {"test-all" ["with-profile" "default:1.3,default:1.4,default:1.5,default:1.6,default:1.8,default" "test"]})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :aliases {"test-all" ["with-profile" "default:1.3,default:1.4,default:1.5,default:1.6,default:1.7,default" "test"]})
