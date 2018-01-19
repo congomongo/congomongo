f01db0e41fb675ff66a582b4f943794aa213faf7
Sean Corfield <sean@corfield.org>
2018-01-19 10:51:03 -0800

    Tidy up test dependencies for 0.5.1 release

--- project.clj
+++ project.clj
@@ -11,13 +11,15 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/data.json "0.2.6"]
                  [org.mongodb/mongo-java-driver "2.14.2"]
-                 [org.clojure/clojure "1.8.0" :scope "provided"]]
+                 [org.clojure/clojure "1.9.0" :scope "provided"]]
   ;; if a :dev profile is added, remember to update :aliases below to
   ;; use it in each with-profile group!
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.9 {:repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-                   :dependencies [[org.clojure/clojure "1.9.0"]]}}
-  :aliases {"test-all" ["with-profile" "default:1.4,default:1.5,default:1.6,default:1.7,default,default:1.9" "test"]})
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+                   :dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "default:1.4,default:1.5,default:1.6,default:1.7,default,default:1.8:default,1.9:default,1.10" "test"]})
