ccf1e05e94deabdc679b6945119e45e5dfe0760f
Johan Heander <johan@timezynk.com>
2018-08-30 08:58:04 +0200

    v0.5.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject congomongo
-  "0.5.2"
+  "0.5.3"
   :description "Clojure-friendly API for MongoDB"
   :url "https://github.com/congomongo/congomongo"
   :mailing-list {:name "CongoMongo mailing list"
